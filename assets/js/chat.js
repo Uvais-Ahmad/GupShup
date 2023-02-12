55ae5d5da8c5e04e40c5d6915905b687188acb47
Uvais Ahmad <uvss21@gmail.com>
2023-02-12 19:08:44 +0530

    Setup done for socketio

---
+++ assets/js/chat.js
@@ -0,0 +1,1 @@
+const socket = io(); //This only single line connect client to server. and this "socket" instance use to listen and fire event
--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
 const app = express();
 const port = 8000;
 
-app.use(express.static('assets'));
+const server = http.createServer(app);  
+const io = socketio(server);    //to initialize socket lib it require server instance
 
+app.use(express.static('assets'));
 //Ejs setup
 app.set('view engine','ejs');
 app.set('views','./views')
 
-// const socketio = require('socket.io');
+io.on('connection' , (socket)=>{
+    console.log("New Connection join",socket.id);
+})
+
+
 
 app.use('/', require('./routers'));
 
-// const io = socketio(app);
-// io.on('connection' , (socket)=>{
-//     console.log("New Connection join ",socket.id);
-// })
-
-app.listen(port , (err)=>{
+server.listen(port , (err)=>{
     if(err){
         console.log("Error occur while running server on port ",port);
     }
